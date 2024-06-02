3cf04ed6259bdcc112c9ac65c69119c83aeb0549
Kradre <me@kradre.com>
2024-06-03 01:53:39 +0700

    Removing building after breaking container

--- src/main/java/com/kradre/buildinboxreborn/SpecialChestListener.java
+++ src/main/java/com/kradre/buildinboxreborn/SpecialChestListener.java
@@ -11,21 +11,31 @@ import com.sk89q.worldedit.function.operation.Operations;
 import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.session.ClipboardHolder;
 import com.sk89q.worldedit.world.block.BlockStateHolder;
+import com.sk89q.worldedit.world.block.BlockTypes;
 import org.bukkit.*;
+import org.bukkit.Color;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.material.MaterialData;
 import org.bukkit.util.BlockVector;
 
+import java.awt.*;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SpecialChestListener implements Listener {
 
@@ -40,9 +50,66 @@ public class SpecialChestListener implements Listener {
                 File schematicFile = new File(BuildInBoxReborn.getInstance().getDataFolder(), item.getItemMeta().getCustomModelData() + ".schem");
                 if(schematicFile.exists()) {
                     editSchematic(block, schematicFile, player);
+                    if(block.getState() instanceof Nameable) {
+                        BlockState state = block.getState();
+                        Nameable nameable = (Nameable) state;
+                        nameable.setCustomName(schematicName + "(" + item.getItemMeta().getCustomModelData() + ")");
+                        state.update();
+                    }
                     //block.setType(Material.AIR);
                 }
             }
+        }
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        Block block = event.getBlock();
+        if (block.getType() == Material.CHEST && block.getState() instanceof Nameable) {
+            Nameable nameable = (Nameable) block.getState();
+            // This should give you the schematic name
+            String schematicName = extractSchematicName(nameable.getCustomName());
+            File schematicFile = new File(BuildInBoxReborn.getInstance().getDataFolder(), schematicName + ".schem");
+            if(schematicFile.exists()) {
+                removeSchematicBlocks(block, schematicFile, event.getPlayer());
+                //Need to apply a new name to the block
+                nameable.setCustomName(schematicName);
+                block.getState().update();
+            }
+        }
+    }
+
+    public void removeSchematicBlocks(Block block, File schematicFile, Player player) {
+        BuiltInClipboardFormat format = BuiltInClipboardFormat.SPONGE_SCHEMATIC;
+        try (ClipboardReader reader = format.getReader(Files.newInputStream(schematicFile.toPath()))) {
+            Clipboard clipboard = reader.read();
+            BlockVector3 minPoint = clipboard.getMinimumPoint();
+            com.sk89q.worldedit.world.World worldEditWorld = BukkitAdapter.adapt(player.getWorld());
+            BlockVector3 chestLocation = BukkitAdapter.asBlockVector(block.getLocation());
+            int delay = 0;
+            player.sendMessage(ChatColor.YELLOW + "Packing back into the black hole...");
+
+            BlockStateHolder airBlockHolder = BlockTypes.AIR.getDefaultState();
+
+            for (BlockVector3 point : clipboard.getRegion()) {
+                BlockVector3 placePosition = point.subtract(minPoint).add(chestLocation);
+                Bukkit.getScheduler().runTaskLater(BuildInBoxReborn.getInstance(),
+                        () -> placeBlockSafely(worldEditWorld, placePosition, airBlockHolder, player), delay++);
+            }
+
+        } catch (IOException e) {
+            player.sendMessage(ChatColor.RED + "Error loading schematic: " + e.getMessage());
+        }
+    }
+
+    public String extractSchematicName(String name) {
+        String regex = "\\((.*?)\\)";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(name);
+        if (matcher.find()) {
+            return matcher.group(1);
+        } else {
+            return null;
         }
     }
 
@@ -59,12 +126,11 @@ public class SpecialChestListener implements Listener {
                     // create location from block vector and play sound
                     Location blockLocation = new Location(bukkitWorld, x, y, z);
                     Material material = data.getMaterial();
-                    if (material != Material.AIR) {
-                        bukkitWorld.playSound(blockLocation, Sound.BLOCK_METAL_PLACE, 1.0F, 1.0F);
-                        // Spawn the block break particles.
-                        Particle.DustOptions dustOptions = new Particle.DustOptions(Color.fromRGB(0, 0, 0), 2);
-                        bukkitWorld.spawnParticle(Particle.SMOKE_NORMAL, blockLocation, 30, dustOptions);
-                    }
+                    bukkitWorld.playSound(blockLocation, (material == Material.AIR) ? Sound.BLOCK_METAL_BREAK : Sound.BLOCK_METAL_PLACE, 1.0F, 1.0F);
+                    // Spawn the block break particles.
+                    Particle.DustOptions dustOptions = new Particle.DustOptions(Color.fromRGB(0, 0, 0), 4);
+                    bukkitWorld.spawnParticle(Particle.SMOKE_NORMAL, blockLocation, 30, dustOptions);
+
 
 
                     // TODO: Make verbose method for those kind of outputs
